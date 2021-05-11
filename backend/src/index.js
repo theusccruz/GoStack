e145dd6f58c13512682626f20d731be04a749bd5
Matheus KBank <matheus8476@outlook.com>
2021-05-11 15:14:54 -0300

    feat: conclusão do primeiro módulo de node

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,16 +1,36 @@
 const express = require('express');
 const app = express();
-const { v4 } = require('uuid'); // módulo uuidv4 está descontinuado
+const { v4: uuidv4, validate: uuidValidate } = require('uuid');
+//const isUUID = require('is-uuid'); usando o is-uuid
 
 app.use(express.json()); // diz para o express interpretar json
 
 const projects = [];
+
 const logRequests = (req, res, next) => {
-    
+   const { method, url } = req;
+   const logLabel = `[${method.toUpperCase()}] ${url}`;
+
+   console.log(logLabel);
+   return next(); // chama próximo middleware
 }
+app.use(logRequests);
+
+const validateProjectId = (req, res, next) => {
+   const { id } = req.params;
+   if (!uuidValidate(id)) {
+      return res.status('400').json({ error: 'Invalid project ID' });      
+   }
+   return next();
+}
+/* app.use abaixo, colocando o prefixo da rota 
+   em que esse middleware será acionado, 
+   nesse caso put ou delete 
+*/
+app.use('/projects/:id', validateProjectId); 
 
 // Routes
-app.get('/projects', (req , res) => {
+app.get('/projects', (req, res) => {
    const { title } = req.query; // pegando query params (?title=BootCamp)
 
    const result = title
@@ -20,13 +40,13 @@ app.get('/projects', (req , res) => {
    return res.json(result);
 });
 
-app.post('/projects', (req , res) => {
+app.post('/projects', (req, res) => {
    const { title, owner } = req.body; // pegando request body (enviado via post)
-   const project = { id: v4(), title, owner };
+   const project = { id: uuidv4(), title, owner };
    /* Seguda forma de adicionar o id ao project
 
       const project = req.body;
-      project['id'] = v4();
+      project['id'] = uuidv4();
    */
 
    projects.push(project);
@@ -38,12 +58,12 @@ app.put('/projects/:id', (req, res) => {
    const { title, owner } = req.body;
 
    const projectIndex = projects.findIndex(project => {
-       return project.id === id;
+      return project.id === id;
    });
 
    if (projectIndex < 0) {
       return res.status('404').json({ error: 'Not found' });
-   } 
+   }
 
    const project = {
       id,
@@ -72,9 +92,9 @@ app.delete('/projects/:id', (req, res) => {
    if (projectIndex < 0) {
       return res.status('404').json({ error: 'Not found' });
    }
-   
+
    projects.splice(projectIndex, 1);
-   return res.status('204').json();    
+   return res.status('204').json();
 })
 
 
