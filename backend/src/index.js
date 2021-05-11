775aa7a348ea14b3a21e11d018d669b36d83dce5
Matheus KBank <matheus8476@outlook.com>
2021-05-11 09:22:39 -0300

    feat: testando parâmetros das rotas do Node

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,18 +1,35 @@
 const express = require('express');
 const app = express();
+app.use(express.json()); // diz para o express interpretar json
 
+const projects = [];
+
+// Routes
 app.get('/projects', (req , res) => {
+   const { title, name } = req.query; // pegando query params (?title=BootCamp)
+   console.log(title, name);
+
    return res.json(
       [
+         {
+            name: name,
+            title: title
+         },
          'Projeto 1',
-         'Projeto 2'
+         'Projeto 2',
       ]
    );
 });
 
 app.post('/projects', (req , res) => {
+   const { title, owner } = req.body; // pegando request body (enviado via post)
+
    return res.json(
       [
+         {
+            app: title,
+            author: owner
+         },
          'Projeto 1',
          'Projeto 2',
          'Projeto: 3'
@@ -21,8 +38,14 @@ app.post('/projects', (req , res) => {
 });
 
 app.put('/projects/:id', (req, res) => {
+   const { id } = req.params; // pegando route params (projects/2/)
+   console.log(id);
+
    return res.json(
       [
+         {
+            idSolicitado: id
+         },
          'Projeto 4',
          'Projeto 2',
          'Projeto: 3'
