02438c7bb20890b42b47b4a09a754d8d6bd7cf8b
Matheus KBank <matheus8476@outlook.com>
2021-05-11 17:41:18 -0300

    feat: criação de parte frontend com React, Babel e Webpack

---
+++ frontend/babel.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    presets: [
+        '@babel/preset-env',// converte funcionalidades de acordo com o ambiente
+        '@babel/preset-react'// converte funcionalidades do react
+    ]
+};
---
+++ frontend/webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+
+module.exports = {
+    entry: path.resolve(__dirname, 'src', 'index.js'), // arquivo de entrada
+    output: {
+        path: path.resolve(__dirname, 'public'), // arquivo convertido
+        filename: 'bundle.js'
+    },
+    devServer: {
+        // passa pro webpack o caminho dos arquivos públicos da aplicação
+        // assim o webpack server vai monitorar modificações
+        contentBase: path.resolve(__dirname, 'public'),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/, // arquivos que vieram pelo node_modules não passam pelo webpack
+                use: {
+                    loader: 'babel-loader'
+                }
+            }
+        ]
+    }
+}
